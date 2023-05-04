b15c137f7166ff03fcd5779d10179d17dc12e7e7
LaurenceB28 <laurence.brd@gmail.com>
2023-05-04 14:47:39 +0200

    formulaires html css terminés

--- views/user/contactForm.php
+++ views/user/contactForm.php
@@ -1,18 +1,31 @@
 <form class="form" action="" method="get">
-    <h2>CONTACT<div class="logoCenter">
-            <img id="logoForm" src="/public/assets/img/logo/LOGO_Food_Fiction.png" id="logo" alt="" />
+    <div class="col-12 col-md-6">
+        <div class="card bg-dark text-white">
+            <div class="card-body text-center">
+                <div class="mb-md-5 mt-md-4 pb-5">
+                    <h2 class="fw-bold mb-2 text-uppercase">Contact
+                        <div class="logoCenter">
+                            <img src="/public/assets/img/logo/LOGO_Food_Fiction.png" id="logo" alt="" />
+                        </div>
+                    </h2>
+                    <div class="col p-2">
+                        <label class="form-label" for="Email">Email</label>
+                        <input type="Email" name="Email" id="Email" placeholder="Email" class="form-control" />
+                    </div>
+                    <div class="col p-2">
+                        <label for="exampleFormControlTextarea1" name="Contact" id="Contact" class="form-label">Votre recette ICI :</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Votre recette ..." rows="8"></textarea>
+                    </div>
+                    <p>Ou votre recette ICI :</p>
+                    <input type="file" id="file" name="file" accept="image/png, image/jpeg, image/pdf">
+                    <div  class="col p-2">
+                        <button class="btn btn-outline-warning btn-md px-3" type="submit">Envoyer</button>
+                    </div>
+                    <div>
+                        <p class="mb-0">Pas encore de compte? <a href="/controllers/contactFormCtrl.php" class="text-white-50 fw-bold">S'inscrire</a></p>
+                    </div>
+                </div>
+            </div>
         </div>
-    </h2>
-
-    <div class="mb-3" id="center">
-        <label for="exampleFormControlInput1" class="form-label">Adresse mail</label>
-        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
     </div>
-
-    <div class="mb-3" id="center">
-        <label for="exampleFormControlTextarea1" class="form-label">La recette manquante ici</label>
-        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-    </div>
-    <label for="file" id="files">Ou votre recette films et séries ici:</label></br>
-    <input type="file" id="file" name="file" accept="image/png, image/jpeg , image/pdf">
 </form>
--- views/user/signUp.php
+++ views/user/signUp.php
@@ -1,41 +1,41 @@
+<form id="form" action="" method="get">
+    <div class="col-12 col-md-8 col-lg-4">
+        <div class="card bg-dark text-white" style="border-radius: 1rem;">
+            <div class="card-body p-5 text-center">
+                <div class="mb-md-5 mt-md-4 pb-5">
+                    <div class="logo-center">
+                        <img id="logo" src="/public/assets/img/logo/LOGO_Food_Fiction.png" alt="" />
+                    </div>
+                    <h2 class="fw-bold mb-2 text-uppercase">Inscrivez vous
 
-    <div class="form" action="">
-        <div class="logoCenter">
-            <img id="logoForm" src="public/assets/img/logo/LOGO_Food_Fiction.png" id="logoNav" alt="logo" />
+                    </h2>
+                    <div class="col p-2">
+                        <label class="form-label" for="Firstname">Prénom</label>
+                        <input type="text" name="Firstname" class="form-control" placeholder="Firstname">
+                    </div>
+                    <div class="col p-2">
+                        <label class="form-label" for="Lastname">Nom de famille</label>
+                        <input type="text" name="Lastname" class="form-control" placeholder="Lastname">
+                    </div>
+
+                    <div class="col p-2">
+                        <label class="form-label" for="Email">Email</label>
+                        <input type="Email" name="Email" id="Email" placeholder="Email" class="form-control" />
+                    </div>
+                    <div class="col p-2">
+                        <label class="form-label" for="typePasswordX">Mot de passe</label>
+                        <input type="password" name="password" id="password" placeholder="Mot de passe" class="form-control" />
+                    </div>
+                    <div class="col p-2">
+                        <label class="form-label" for="typePasswordX">Confirmer votre mot de passe</label>
+                        <input type="password" name="confirm_password" id="confirm_password" placeholder="Confirmer votre mot de passe" class="form-control" required />
+                    </div>
+                    <div class="col p-2">
+                        <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">mot de passe oublié?</a></p>
+                    </div>
+                    <button class="btn btn-outline-warning btn-lg px-5" type="submit">connexion</button>
+                </div>
+            </div>
         </div>
-        <div class="title">Bienvenue</div>
-        <div class="subtitle">Créez un compte FICTION!</div>
-        <div class="input-container ic1">
-            <input id="firstname" class="input" type="text" placeholder=" " />
-            <div class="cut"></div>
-            <label for="firstname" class="placeholder">Prénom</label>
-        </div>
-        <div class="input-container ic2">
-            <input id="lastname" class="input" type="text" placeholder=" " />
-            <div class="cut"></div>
-            <label for="lastname" class="placeholder">Nom de famille</label>
-        </div>
-        <div class="input-container ic2">
-            <input id="lastname" class="input" type="text" placeholder=" " />
-            <div class="cut"></div>
-            <label for="lastname" class="placeholder">Mot de passe</label>
-        </div>
-        <div class="input-container ic2">
-            <input id="lastname" class="input" type="text" placeholder=" " />
-            <div class="cut"></div>
-            <label for="lastname" class="placeholder">Confimer votre mot de passe</label>
-        </div>
-        <div class="input-container ic2">
-            <input id="email" class="input" type="text" placeholder=" " />
-            <div class="cut cut-short"></div>
-            <label for="email" class="placeholder">Email</label>
-        </div>
-        <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-            <label class="form-check-label" for="exampleCheck1">j'accepte les conditions du sites</label>
-        </div>
-        <button type="text" class="submit">Envoyer</button>
     </div>
-</body>
-
-</html>
+</form>
