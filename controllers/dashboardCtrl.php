b353a9ec9ca7d7fa1245ac3d6e78b664b9bc88c6
LaurenceB28 <laurence.brd@gmail.com>
2023-05-22 14:07:04 +0200

    jrhori

--- controllers/dashboardCtrl.php
+++ controllers/dashboardCtrl.php
@@ -3,5 +3,5 @@ $footer = true;
 $stylesheet = 'dashboard.css';
 include(__DIR__ . '/../views/templates/header.php');
 include(__DIR__ . '/../views/user/dashboard.php');
-include(__DIR__ . '/../views/templates/footer.php');
+// include(__DIR__ . '/../views/templates/footer.php');
 
